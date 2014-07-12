2f63a4d0f6027cb7faf40bfdb7ef63ff4ba91a72
Matthias <tqzamf@gmail.com>
2014-07-12 19:59:29 +0200

    disable unused modules at boot to save some power

---
+++ arch/mips/pnx8550/stb810/powersave.c
@@ -0,0 +1,40 @@
+/*
+ * Non-driver to power down all unused peripherals.
+ * 
+ * Public domain.
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <cm.h>
+#include <dcsn.h>
+
+static int __init pnx8550_powerdown_unused(void)
+{
+#define POWERDOWN(modname) \
+	PNX8550_DCSN_POWERDOWN_CTL(PNX8550_ ## modname ## _BASE) \
+			= PNX8550_DCSN_POWERDOWN_CMD; \
+	PNX8550_CM_ ## modname ## _CTL = 0;
+	
+	// smart card 2 and internal OHCI. both are unused because they aren't
+	// connected anywhere, but probably don't use much power anyway.
+	POWERDOWN(SC2);
+	POWERDOWN(OHCI);
+	
+	// MPEG routing and decoding hardware. unused without tuners.
+	POWERDOWN(VMPG);
+	POWERDOWN(VLD);
+	POWERDOWN(MSP1);
+	POWERDOWN(MSP2);
+	// (analog) video input hardware. the board never had analog video in
+	// the first place, so these have always been unused.
+	POWERDOWN(VIP1);
+	POWERDOWN(VIP2);
+	// memory-based scalers. not needed when there is no video decoding.
+	POWERDOWN(MBS1);
+	POWERDOWN(MBS2);
+	POWERDOWN(MBS3);
+
+    return 0;
+}
+subsys_initcall(pnx8550_powerdown_unused);
