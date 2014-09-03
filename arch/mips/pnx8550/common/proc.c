ad80defd22873c8caa415de708aa90928f540f68
Matthias <tqzamf@gmail.com>
2014-09-03 22:08:46 +0200

    reduce code duplication in register dump code
    this code is inherently very repetitive, and because the register number is
    hardcoded into the mfc0 instruction, the code cannot use a loop.
    however, it can still use a macro to at least reduce the ducplication at
    source code level.

--- arch/mips/pnx8550/common/proc.c
+++ arch/mips/pnx8550/common/proc.c
@@ -51,33 +51,40 @@ static int pnx8550_registers_read(char* page, char** start, off_t offset, int co
         int len = 0;
 
         if (offset==0) {
-                len += sprintf(&page[len], "prid:      %#10.8x\n", read_c0_prid());
-                len += sprintf(&page[len], "config:    %#10.8x\n", read_c0_config());
-                len += sprintf(&page[len], "config1:   %#10.8x\n", read_c0_config1());
-                len += sprintf(&page[len], "config2:   %#10.8x\n", read_c0_config2());
-                len += sprintf(&page[len], "config3:   %#10.8x\n", read_c0_config3());
-                len += sprintf(&page[len], "configPR:  %#10.8x\n", read_c0_config7());
+        #define DUMP_NAMED(reg, name) \
+                len += sprintf(&page[len], "%-10s %#10.8x\n", name ":", read_c0_##reg())
+        #define DUMP(reg) \
+                len += sprintf(&page[len], "%-10s %#10.8x\n", #reg ":", read_c0_##reg())
+        #define DUMP_LONG(reg) \
+                len += sprintf(&page[len], "%-10s %#10.8lx\n", #reg ":", read_c0_##reg())
 
-                len += sprintf(&page[len], "status:    %#10.8x\n", read_c0_status());
-                len += sprintf(&page[len], "cause:     %#10.8x\n", read_c0_cause());
-                len += sprintf(&page[len], "debug:     %#10.8x\n", read_c0_debug());
-                len += sprintf(&page[len], "wired:     %#10.8x\n", read_c0_wired());
-                len += sprintf(&page[len], "pagemask:  %#10.8x\n", read_c0_pagemask());
-                len += sprintf(&page[len], "pagegrain: %#10.8x\n", read_c0_pagegrain());
+                DUMP(prid);
+                DUMP(config);
+                DUMP(config1);
+                DUMP(config2);
+                DUMP(config3);
+                DUMP_NAMED(config7, "configPR");
 
-                len += sprintf(&page[len], "count:     %#10.8x\n", read_c0_count());
-                len += sprintf(&page[len], "count_2:   %#10.8x\n", read_c0_count2());
-                len += sprintf(&page[len], "count_3:   %#10.8x\n", read_c0_count3());
-                len += sprintf(&page[len], "compare:   %#10.8x\n", read_c0_compare());
-                len += sprintf(&page[len], "compare_2: %#10.8x\n", read_c0_compare2());
-                len += sprintf(&page[len], "compare_3: %#10.8x\n", read_c0_compare3());
+                DUMP(status);
+                DUMP(cause);
+                DUMP(debug);
+                DUMP(wired);
+                DUMP(pagemask);
+                DUMP(pagegrain);
 
-                len += sprintf(&page[len], "cmem_0:    %#10.8x\n", read_c0_diag4());
-                len += sprintf(&page[len], "cmem_1:    %#10.8x\n", read_c0_diag5());
-                len += sprintf(&page[len], "cmem_2:    %#10.8x\n", read_c0_diag6());
-                len += sprintf(&page[len], "cmem_3:    %#10.8x\n", read_c0_diag7());
-                len += sprintf(&page[len], "hwrena:    %#10.8x\n", read_c0_hwrena());
-                len += sprintf(&page[len], "user:      %#10.8lx\n", read_c0_userlocal());
+                DUMP(count);
+                DUMP(count2);
+                DUMP(count3);
+                DUMP(compare);
+                DUMP(compare2);
+                DUMP(compare3);
+
+                DUMP_NAMED(diag4, "cmem0");
+                DUMP_NAMED(diag5, "cmem1");
+                DUMP_NAMED(diag6, "cmem2");
+                DUMP_NAMED(diag7, "cmem3");
+                DUMP(hwrena);
+                DUMP_LONG(userlocal);
         }
 
         return len;
