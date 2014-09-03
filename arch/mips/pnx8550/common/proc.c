b3e2c5eb097fe62c28aaa6620fb13b695c9062e2
Matthias <tqzamf@gmail.com>
2014-09-03 21:47:33 +0200

    add more registers in /proc/pnx8550/registers
    this tries to somewhat consistently include all registers that contain
    implementation-specific configuration, as well as all containing kernel-set
    configuration bits.
    
    it doesn't include registers that are used for cache and TLB management,
    and are expected to change frequently.

--- arch/mips/pnx8550/common/proc.c
+++ arch/mips/pnx8550/common/proc.c
@@ -51,18 +51,33 @@ static int pnx8550_registers_read(char* page, char** start, off_t offset, int co
         int len = 0;
 
         if (offset==0) {
+                len += sprintf(&page[len], "prid:      %#10.8x\n", read_c0_prid());
+                len += sprintf(&page[len], "config:    %#10.8x\n", read_c0_config());
                 len += sprintf(&page[len], "config1:   %#10.8x\n", read_c0_config1());
                 len += sprintf(&page[len], "config2:   %#10.8x\n", read_c0_config2());
                 len += sprintf(&page[len], "config3:   %#10.8x\n", read_c0_config3());
                 len += sprintf(&page[len], "configPR:  %#10.8x\n", read_c0_config7());
+
                 len += sprintf(&page[len], "status:    %#10.8x\n", read_c0_status());
                 len += sprintf(&page[len], "cause:     %#10.8x\n", read_c0_cause());
+                len += sprintf(&page[len], "debug:     %#10.8x\n", read_c0_debug());
+                len += sprintf(&page[len], "wired:     %#10.8x\n", read_c0_wired());
+                len += sprintf(&page[len], "pagemask:  %#10.8x\n", read_c0_pagemask());
+                len += sprintf(&page[len], "pagegrain: %#10.8x\n", read_c0_pagegrain());
+
                 len += sprintf(&page[len], "count:     %#10.8x\n", read_c0_count());
                 len += sprintf(&page[len], "count_2:   %#10.8x\n", read_c0_count2());
                 len += sprintf(&page[len], "count_3:   %#10.8x\n", read_c0_count3());
                 len += sprintf(&page[len], "compare:   %#10.8x\n", read_c0_compare());
                 len += sprintf(&page[len], "compare_2: %#10.8x\n", read_c0_compare2());
                 len += sprintf(&page[len], "compare_3: %#10.8x\n", read_c0_compare3());
+
+                len += sprintf(&page[len], "cmem_0:    %#10.8x\n", read_c0_diag4());
+                len += sprintf(&page[len], "cmem_1:    %#10.8x\n", read_c0_diag5());
+                len += sprintf(&page[len], "cmem_2:    %#10.8x\n", read_c0_diag6());
+                len += sprintf(&page[len], "cmem_3:    %#10.8x\n", read_c0_diag7());
+                len += sprintf(&page[len], "hwrena:    %#10.8x\n", read_c0_hwrena());
+                len += sprintf(&page[len], "user:      %#10.8lx\n", read_c0_userlocal());
         }
 
         return len;
