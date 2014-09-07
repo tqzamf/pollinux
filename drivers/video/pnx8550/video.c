d106979ece31c98bd28cf1aa539e83c6286bdf66
Matthias <tqzamf@gmail.com>
2014-09-07 20:59:21 +0200

    document and reformat Anabel (PNX8510) initialization
    instead of (essentially) a memory dump, use an annotated series of function
    calls. this is slightly larger and slower, but way more readable.

--- drivers/video/pnx8550/video.c
+++ drivers/video/pnx8550/video.c
@@ -26,6 +26,7 @@
 #include <audio.h>
 #include <linux/i2c.h>
 #include <i2c.h>
+#include <dcsn.h>
 #include <cm.h>
 
 #define I2C_SCART_BUS                  PNX8550_I2C_IP0105_BUS1
@@ -36,124 +37,139 @@
 #define I2C_ANABEL_AUDIO1_ADDR         0x76
 #define I2C_ANABEL_AUDIO2_ADDR         0x7e
 
-/* Register / Data pairs used to initialise Anabel into PAL mode */
-static const unsigned char pnx8550fb_anabel_pal[] =
+// sets a single register over I²C
+static void pnx8550fb_set_reg(unsigned char addr, unsigned char reg, unsigned char value)
 {
-    0x27,0x00,
-    0x28,0x21,
-    0x29,0x1d,
-    0x2b,0xb8,
-    0x2c,0x00,
-    0x54,0x00,
-    0x5a,0x00,
-    0x60,0x00,
-    0x61,0x06,
-    0x63,0xcb,
-    0x64,0x8a,
-    0x65,0x09,
-    0x66,0x2a,
-    0x67,0x00,
-    0x68,0x00,
-    0x69,0x00,
-    0x6a,0x00,
-    0x6c,0x02,
-    0x6d,0x22,
-    0x6e,0x60,
-    0x6f,0x00,
-    0x70,0x1a,
-    0x71,0x9f,
-    0x72,0x61,
-    0x7a,0x16,
-    0x7b,0x37,
-    0x7c,0x40,
-    0x7d,0x00,
-    0x7f,0x00,
-    0x7f,0x00,
-    0xa0,0x00,
-    0xa2,0x10,
-    0xa3,0x80,
-    0xa4,0x80,
-    0xba,0x70,
-    0x5b,0x89,
-    0x5c,0xbe,
-    0x5d,0x37,
-    0x5e,0x39,
-    0x5f,0x39,
-    0x62,0x38,
-    0xc2,0x1f,
-    0xc3,0x0A,
-    0xc4,0x0A,
-    0xc5,0x0A,
-    0xc6,0x00,
-    0x2d,0x40,
-    0x3a,0x48,
-    0x95,0x00,
-    0x15,0x30,
-    0xa0,0x00,
-    0x20,0x00,
-    0x6e,0x00
-};
+	struct i2c_adapter *adapter;
+    unsigned char data[2] = { reg, value };
+	struct i2c_msg msg = {
+		.addr = addr,
+		.flags = 0,
+		.len = 2,
+		.buf = data,
+	};
+	
+    adapter = i2c_get_adapter(I2C_ANABEL_BUS);
+	if (i2c_transfer(adapter, &msg, 1) != 1)
+		printk(KERN_ERR "%s: error writing device %02x register %02x\n",
+				__func__, addr, reg);
+}
 
-/* Register / Data pairs used to initialise Anabel into NTSC mode */
-static const unsigned char pnx8550fb_anabel_ntsc[] =
+// sets a single register in the primary video channel of ANABEL
+static void pnx8550fb_anabel_set_reg(unsigned char reg, unsigned char value)
 {
-    0x27,0x00,
-    0x28,0x25,
-    0x29,0x1d,
-    0x2c,0x00,
-    0x54,0x00,
-    0x5a,0x88,
-    0x60,0x00,
-    0x61,0x11,
-    0x63,0x1f,
-    0x64,0x7c,
-    0x65,0xf0,
-    0x66,0x21,
-    0x6c,0xf2,
-    0x6d,0x03,
-    0x6e,0xD0,
-    0x6f,0x00,
-    0x70,0xfb,
-    0x71,0x90,
-    0x72,0x60,
-    0x7a,0x00,
-    0x7b,0x05,
-    0x7c,0x40,
-    0x7d,0x00,
-    0xa0,0x00,
-    0xa2,0x0d,
-    0xa3,0x80,
-    0xa4,0x80,
-    0xba,0x70,
-    0x5b,0x7d,
-    0x5c,0xaf,
-    0x5d,0x3e,
-    0x5e,0x32,
-    0x5f,0x32,
-    0x62,0x4c,
-    0xc2,0x1e,
-    0xc3,0x0A,
-    0xc4,0x0A,
-    0xc5,0x0A,
-    0xc6,0x01,
-    0x2d,0x40,
-    0x3a,0x48,
-    0x95,0x00,
-    0x15,0x30,
-    0xa0,0x00,
-    0x20,0x00,
-    0x6e,0x00
-};
+	pnx8550fb_set_reg(I2C_ANABEL_ADDR, reg, value);
+}
 
-/* commands to blank and unblank the Anabel */
-static const unsigned char pnx8550fb_anabel_blank[] =
+// Function used to set up PAL/NTSC in Anabel
+static void pnx8550fb_setup_anabel(int pal)
 {
-    0x6e,0x40
-};
-static const unsigned char pnx8550fb_anabel_unblank[] =
-{
-    0x6e,0x00
-};
+	// make sure the clocks are enabled
+	pnx8550fb_set_reg(I2C_ANABEL_AUDIO1_ADDR, 0x01, 0x00);
+	pnx8550fb_set_reg(I2C_ANABEL_AUDIO1_ADDR, 0x02, 0x00);
+	
+	// the PNX8510 datasheet lists all undefined registers as "Must be
+	// initialized to zero". these are the ones that are actually
+	// initialized in the NXP splash screen code; the rest probably
+	// doesn't matter.
+	pnx8550fb_anabel_set_reg(0x60, 0x00);
+	pnx8550fb_anabel_set_reg(0x7d, 0x00);
+	
+	// disable insertion of wide screen signalling, copy guard,
+	// video progamming system, closed captioning & teletext
+	pnx8550fb_anabel_set_reg(0x27, 0x00);
+	pnx8550fb_anabel_set_reg(0x2c, 0x00);
+	pnx8550fb_anabel_set_reg(0x54, 0x00);
+	pnx8550fb_anabel_set_reg(0x6f, 0x00);
+	
+    if (pal) {
+		// color burst config
+		pnx8550fb_anabel_set_reg(0x28, 0x21);
+		pnx8550fb_anabel_set_reg(0x29, 0x1d);
+		pnx8550fb_anabel_set_reg(0x5a, 0x00);
+		// chroma encoding config
+		pnx8550fb_anabel_set_reg(0x61, 0x06);
+		pnx8550fb_anabel_set_reg(0x63, 0xcb);
+		pnx8550fb_anabel_set_reg(0x64, 0x8a);
+		pnx8550fb_anabel_set_reg(0x65, 0x09);
+		pnx8550fb_anabel_set_reg(0x66, 0x2a);
+		pnx8550fb_anabel_set_reg(0x6c, 0x02);
+		pnx8550fb_anabel_set_reg(0x6d, 0x22);
+		pnx8550fb_anabel_set_reg(0x6e, 0x60);
+		// screen geometry
+		pnx8550fb_anabel_set_reg(0x70, 0x1a);
+		pnx8550fb_anabel_set_reg(0x71, 0x9f);
+		pnx8550fb_anabel_set_reg(0x72, 0x61);
+		pnx8550fb_anabel_set_reg(0x7a, 0x16);
+		pnx8550fb_anabel_set_reg(0x7b, 0x37);
+		pnx8550fb_anabel_set_reg(0x7c, 0x40);
+		// YUV encoding & pedestal
+		pnx8550fb_anabel_set_reg(0xa0, 0x00);
+		pnx8550fb_anabel_set_reg(0xa2, 0x10);
+		pnx8550fb_anabel_set_reg(0xa3, 0x80);
+		pnx8550fb_anabel_set_reg(0xa4, 0x80);
+		pnx8550fb_anabel_set_reg(0x5b, 0x89);
+		pnx8550fb_anabel_set_reg(0x5c, 0xbe);
+		pnx8550fb_anabel_set_reg(0x5d, 0x37);
+		pnx8550fb_anabel_set_reg(0x5e, 0x39);
+		pnx8550fb_anabel_set_reg(0x5f, 0x39);
+		pnx8550fb_anabel_set_reg(0x62, 0x38);
+		// DAC output levels
+		pnx8550fb_anabel_set_reg(0xc2, 0x1f);
+		pnx8550fb_anabel_set_reg(0xc3, 0x0A);
+		pnx8550fb_anabel_set_reg(0xc4, 0x0A);
+		pnx8550fb_anabel_set_reg(0xc5, 0x0A);
+		pnx8550fb_anabel_set_reg(0xc6, 0x00);
+	} else {
+		// color burst config
+		pnx8550fb_anabel_set_reg(0x28, 0x25);
+		pnx8550fb_anabel_set_reg(0x29, 0x1d);
+		pnx8550fb_anabel_set_reg(0x5a, 0x88);
+		// chroma encoding config
+		pnx8550fb_anabel_set_reg(0x61, 0x11);
+		pnx8550fb_anabel_set_reg(0x63, 0x1f);
+		pnx8550fb_anabel_set_reg(0x64, 0x7c);
+		pnx8550fb_anabel_set_reg(0x65, 0xf0);
+		pnx8550fb_anabel_set_reg(0x66, 0x21);
+		pnx8550fb_anabel_set_reg(0x6c, 0xf2);
+		pnx8550fb_anabel_set_reg(0x6d, 0x03);
+		pnx8550fb_anabel_set_reg(0x6e, 0xD0);
+		// screen geometry
+		pnx8550fb_anabel_set_reg(0x70, 0xfb);
+		pnx8550fb_anabel_set_reg(0x71, 0x90);
+		pnx8550fb_anabel_set_reg(0x72, 0x60);
+		pnx8550fb_anabel_set_reg(0x7a, 0x00);
+		pnx8550fb_anabel_set_reg(0x7b, 0x05);
+		pnx8550fb_anabel_set_reg(0x7c, 0x40);
+		// YUV encoding & pedestal
+		pnx8550fb_anabel_set_reg(0xa0, 0x00);
+		pnx8550fb_anabel_set_reg(0xa2, 0x0d);
+		pnx8550fb_anabel_set_reg(0xa3, 0x80);
+		pnx8550fb_anabel_set_reg(0xa4, 0x80);
+		pnx8550fb_anabel_set_reg(0x5b, 0x7d);
+		pnx8550fb_anabel_set_reg(0x5c, 0xaf);
+		pnx8550fb_anabel_set_reg(0x5d, 0x3e);
+		pnx8550fb_anabel_set_reg(0x5e, 0x32);
+		pnx8550fb_anabel_set_reg(0x5f, 0x32);
+		pnx8550fb_anabel_set_reg(0x62, 0x4c);
+		// DAC output levels
+		pnx8550fb_anabel_set_reg(0xc2, 0x1e);
+		pnx8550fb_anabel_set_reg(0xc3, 0x0A);
+		pnx8550fb_anabel_set_reg(0xc4, 0x0A);
+		pnx8550fb_anabel_set_reg(0xc5, 0x0A);
+		pnx8550fb_anabel_set_reg(0xc6, 0x01);
+	}
+	
+	// don't perform signature analysis
+	pnx8550fb_anabel_set_reg(0xba, 0x70);
+	// DAC configured for CVBS + RGB (instead of Y/C)
+	pnx8550fb_anabel_set_reg(0x2d, 0x40);
+	// set SD (not HD) mode; interface config
+	pnx8550fb_anabel_set_reg(0x3a, 0x48);
+	pnx8550fb_anabel_set_reg(0x95, 0x00);
+	// unblank the screen
+	pnx8550fb_anabel_set_reg(0x6e, 0x00);
+}
 
 /* Register / Data information used to initialise SCART switch. */
 static const unsigned char pnx8550fb_scart_data[] = {
@@ -240,53 +256,17 @@ void pnx8550fb_set_volume(int volume)
 		printk(KERN_ERR "%s: write error\n", __func__);
 }
 
-static struct i2c_msg pnx8550fb_anabel_msg = {
-	.addr = I2C_ANABEL_ADDR,
-	.flags = 0,
-	.len = 2,
-};
-
-/* Function used to set up PAL/NTSC using Anabel */
-static void pnx8550fb_setup_anabel(int pal)
-{
-    int i, len;
-    const unsigned char *data;
-    struct i2c_adapter *adapter = i2c_get_adapter(I2C_ANABEL_BUS);
-
-    if (pal) {
-		data = pnx8550fb_anabel_pal;
-		len = sizeof(pnx8550fb_anabel_pal);
-	} else {
-		data = pnx8550fb_anabel_ntsc;
-		len = sizeof(pnx8550fb_anabel_ntsc);
-	}
-
-	for(i=0; i<len; i+=2)
-	{
-		pnx8550fb_anabel_msg.buf = (unsigned char *) &data[i];
-		if ((i2c_transfer(adapter, &pnx8550fb_anabel_msg, 1)) != 1) {
-			printk(KERN_ERR "%s: write error at %d\n", __func__, i);
-			break;
-		}
-	}
-}
-
-/* Function used to set up PAL/NTSC using Anabel */
+// enable or disable blanking
 void pnx8550fb_set_blanking(int blank)
 {
-    struct i2c_adapter *adapter = i2c_get_adapter(I2C_ANABEL_BUS);
-
     if (blank)
-		pnx8550fb_anabel_msg.buf = (unsigned char *) pnx8550fb_anabel_blank;
+		pnx8550fb_anabel_set_reg(0x6e, 0x40);
 	else
-		pnx8550fb_anabel_msg.buf = (unsigned char *) pnx8550fb_anabel_unblank;
-
-	if ((i2c_transfer(adapter, &pnx8550fb_anabel_msg, 1)) != 1)
-		printk(KERN_ERR "%s: write error\n", __func__);
+		pnx8550fb_anabel_set_reg(0x6e, 0x00);
 }
 
 /* Function used to set up PAL/NTSC using the QVCP */
-static void pnx8550fb_setup_QVCP(unsigned int buffer, int pal)
+static void pnx8550fb_setup_qvcp(unsigned int buffer, int pal)
 {
     // start PLL & DDS
     PNX8550_CM_PLL2_CTL = PNX8550_CM_PLL_27MHZ;
@@ -374,7 +354,7 @@ static void pnx8550fb_setup_QVCP(unsigned int buffer, int pal)
 }
 
 /* Shuts down the QVCP by powering it down */
-static void pnx8550fb_shutdown_QVCP(void)
+static void pnx8550fb_shutdown_qvcp(void)
 {
 	// disable timing generator, and thus all layers
     PNX8550FB_QVCP1_REG(0x020) = 0x00000000;
@@ -387,53 +367,16 @@ static void pnx8550fb_shutdown_QVCP(void)
     PNX8550_CM_PLL2_CTL = PNX8550_CM_PLL_POWERDOWN;
 }
 
-/* command to shut down the unused second video channel on the Anabel */
-static const unsigned char pnx8550fb_anabel2_shutdown[] =
-{
-    0xa5,0x01
-};
-/* command to shut down the unused audio channels on the Anabel */
-static const unsigned char pnx8550fb_anabel_audio_shutdown[] =
-{
-    0xfe,0x00
-};
-/* command to shut down the clocks to the second video channel on the Anabel */
-static const unsigned char pnx8550fb_anabel_clock_shutdown[] =
-{
-    0x01,0x01,0x01
-};
-
-static struct i2c_msg pnx8550fb_anabel2_msg = {
-	.flags = 0,
-};
-
 /* shuts down the secondary QVCP and the unused parts of Anabel */
 static void pnx8550fb_shutdown_unused(void)
 {
-    struct i2c_adapter *adapter = i2c_get_adapter(I2C_ANABEL_BUS);
-
-	// shutdown PNX8510 second video channel
-	pnx8550fb_anabel2_msg.buf = (unsigned char *) pnx8550fb_anabel2_shutdown;
-	pnx8550fb_anabel2_msg.addr = I2C_ANABEL2_ADDR;
-	if ((i2c_transfer(adapter, &pnx8550fb_anabel2_msg, 1)) != 1)
-		printk(KERN_ERR "%s: write error for VIDEO2\n", __func__);
-
+	// shutdown PNX8510 second video channel, including clocks
+	pnx8550fb_set_reg(I2C_ANABEL2_ADDR, 0xa5, 0x01);
+	pnx8550fb_set_reg(I2C_ANABEL_AUDIO2_ADDR, 0x01, 0x01);
+	pnx8550fb_set_reg(I2C_ANABEL_AUDIO2_ADDR, 0x02, 0x01);
 	// shutdown both PNX8510 audio channels
-	pnx8550fb_anabel2_msg.buf = (unsigned char *) pnx8550fb_anabel_audio_shutdown;
-	pnx8550fb_anabel2_msg.len = 2;
-	pnx8550fb_anabel2_msg.addr = I2C_ANABEL_AUDIO1_ADDR;
-	if ((i2c_transfer(adapter, &pnx8550fb_anabel2_msg, 1)) != 1)
-		printk(KERN_ERR "%s: write error for AUDIO1\n", __func__);
-	pnx8550fb_anabel2_msg.addr = I2C_ANABEL_AUDIO2_ADDR;
-	if ((i2c_transfer(adapter, &pnx8550fb_anabel2_msg, 1)) != 1)
-		printk(KERN_ERR "%s: write error for AUDIO2\n", __func__);
-
-	// shutdown clocks to PNX8510 second video channel
-	pnx8550fb_anabel2_msg.buf = (unsigned char *) pnx8550fb_anabel_clock_shutdown;
-	pnx8550fb_anabel2_msg.len = 3;
-	pnx8550fb_anabel2_msg.addr = I2C_ANABEL_AUDIO2_ADDR;
-	if ((i2c_transfer(adapter, &pnx8550fb_anabel2_msg, 1)) != 1)
-		printk(KERN_ERR "%s: write error for CLOCK2\n", __func__);
+	pnx8550fb_set_reg(I2C_ANABEL_AUDIO1_ADDR, 0xfe, 0x00);
+	pnx8550fb_set_reg(I2C_ANABEL_AUDIO2_ADDR, 0xfe, 0x00);
 
     // Make sure the QVCP's output clock is enabled. If it isn't, any
     // access to the DACs will hang the system, regardless of any
@@ -459,7 +402,7 @@ static void pnx8550fb_shutdown_unused(void)
 void pnx8550fb_setup_display(unsigned int base, int pal)
 {
     /* Set up the QVCP registers */
-    pnx8550fb_setup_QVCP(base, pal);
+    pnx8550fb_setup_qvcp(base, pal);
 
     /* Set up Anabel using I2C */
     pnx8550fb_setup_anabel(pal);
@@ -475,7 +418,7 @@ void pnx8550fb_setup_display(unsigned int base, int pal)
 void pnx8550fb_shutdown_display(void)
 {
     /* power down the QVCP */
-    pnx8550fb_shutdown_QVCP();
+    pnx8550fb_shutdown_qvcp();
 
     /* Shut down the Scart switch */
     pnx8550fb_shutdown_scart();
