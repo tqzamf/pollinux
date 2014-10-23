5b6f8960d0dd2f3aa4dd3572d587a495be6078c3
Matthias <tqzamf@gmail.com>
2014-10-23 23:13:18 +0200

    document power-down with both GPIO12 conventions
    keeping GPIO12 high until reset allows rebooting with the default boot
    script, but doesn't allow receiving any frontpanel activity. worse still,
    any frontpanel activity except for the power button will cause power-down
    to fail; the board will reboot instead.
    
    setting GPIO12 low in the boot script allows rebooting without losing the
    frontpanel buttons and IR, but obviously requires modification of the boot
    script.
    
    whichever method is used, the power-down as implemented will work properly.
    document that fact, and why this is so.

--- arch/mips/pnx8550/common/reset.c
+++ arch/mips/pnx8550/common/reset.c
@@ -26,7 +26,7 @@
 #include <asm/delay.h>
 #include <asm/reboot.h>
 #include <glb.h>
-#include <standbyctl.h>
+#include <gpio.h>
 #include <prom.h>
 
 void pnx8550_machine_restart(char *command)
@@ -46,20 +46,31 @@ void pnx8550_machine_halt(void)
 
 void pnx8550_machine_power_off(void)
 {
-	/* GPIO12 controls reset behavior in a complex way. After boot, it is 1,
-	 * meaning that a CPU reset causes a reboot. If it is ever set to 0, the
-	 * machine will instead power off on reset. Setting GPIO12 back to 1
-	 * makes it power off IMMEDIATELY after ~1sec.
-	 * Thus, the method for powering off is simple: set GPIO12 to open-drain
-	 * output (in case something misconfigured it), pulse it low and pull it
-	 * back up immediately. Note that we must leave it high until this point
-	 * for reboot to work as intended. */
-	// FIXME doesn't work if board was powered up by the remote
-	// but nothing works reliably in that case
-	PNX8550_STANDBYCTL_MODE = (PNX8550_STANDBYCTL_MODE & PNX8550_STANDBYCTL_MODE_MASK) | PNX8550_STANDBYCTL_MODE_CONFIG;
-	PNX8550_STANDBYCTL_DATA = PNX8550_STANDBYCTL_ENABLE_POWEROFF;
+	/* GPIO12 controls reset behavior in a complex way: If it is released
+	 * high for >~1sec, after having been set low previously, the board
+	 * powers off. That is, a raising edge on GPIO12 powers off, unless
+	 * the GPIO is set low again during this time.
+	 *
+	 * The default boot script leaves GPIO12 high, so if it is ever set
+	 * low, a CPU reset will release the GPIO and power down the board.
+	 * If the GPIO is kept high, a CPU reset simply causes a reboot.
+	 *
+	 * If the boot script is modified to set GPIO12 low, then a CPU reset
+	 * will release GPIO12, but only for a very short time. Thus the
+	 * board will not power down and perform a normal reboot instead.
+	 * However, releasing GPIO12 without a reset still causes the board
+	 * to power down after ~1sec.
+	 *
+	 * Thus, the method for powering off is simple: set GPIO12 to
+	 * open-drain output (in case something misconfigured it), pull it
+	 * low (in case it hasn't already been set low), and pull it back up
+	 * immediately. This works regardless of what GPIO12's state was
+	 * before.
+	 */
+	PNX8550_GPIO_MODE_PUSHPULL(PNX8550_GPIO_STANDBY);
+	PNX8550_GPIO_SET_LOW(PNX8550_GPIO_STANDBY);
 	udelay(2000);
-	PNX8550_STANDBYCTL_DATA = PNX8550_STANDBYCTL_POWEROFF_NOW;
+	PNX8550_GPIO_SET_HIGH(PNX8550_GPIO_STANDBY);
 
 	// wait for poweroff. if it fails, we busy-loop until the end of time.
 	while (1) {
